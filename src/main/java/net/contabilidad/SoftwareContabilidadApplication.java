62695510865a624fa39cd388d8dcf04e558ab5d9
Juan Agustin Lizarazo Rangel <juan.lizarazo@correo.icesi.edu.co>
2020-04-15 10:08:46 -0500

    Mi primer commit
    In

---
+++ src/main/java/net/contabilidad/SoftwareContabilidadApplication.java
@@ -0,0 +1,13 @@
+package net.contabilidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareContabilidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareContabilidadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/contabilidad/SoftwareContabilidadApplicationTests.java
@@ -0,0 +1,13 @@
+package net.contabilidad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareContabilidadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
